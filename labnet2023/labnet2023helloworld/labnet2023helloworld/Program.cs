a41faa475bed86cce5c0ef7f7ccafc075d1b0d3a
Joaquín Gabriel Lagares García <joacolagares@outlook.com>
2023-08-15 11:43:06 -0300

    Subo codigo Hello World

---
+++ labnet2023/labnet2023helloworld/labnet2023helloworld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labnet2023helloworld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //agrego el readline para que no se cierre la consola
+            Console.ReadLine();
+        }
+    }
+}
