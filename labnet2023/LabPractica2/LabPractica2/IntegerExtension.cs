353b6b50aab2192295857649adff456fd56cd053
Joaquín Gabriel Lagares García <joacolagares@outlook.com>
2023-08-21 18:11:47 -0300

    Solucion practica 2

---
+++ labnet2023/LabPractica2/LabPractica2/IntegerExtension.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabPractica2
+{
+    public static class DoubleExtension
+    {
+        public static void DividirPor0(this int numero)
+        {
+            try
+            {
+                Console.WriteLine(numero / 0);
+                
+            }catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public static void Dividir(this int dividendo, int divisor)
+        {
+            try
+            {
+                if (divisor == 0)
+                {
+                    throw new DivideByZeroException();
+                }
+
+                double res = (double)dividendo / divisor;
+                Console.WriteLine($"El resultado de la operación es: {res}");
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Que onda facha?  o sabias que no se puede dividir entre 0 campeon?.");
+            }
+            finally
+            {
+                Console.WriteLine("Operación intentada");
+            }
+        }
+    }
+
+}
---
+++ labnet2023/LabPractica2/LabPractica2/Logic.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabPractica2
+{
+    public class Logic : Exception
+    {
+
+        public Logic() { }
+     
+        public void ThrowException() { 
+            throw new Exception();
+        }
+        public void ExcepcionPersonalizada()
+        {
+            throw new Exception("excepcion personalizada");
+        }
+    }
+}
